f6763a3ae51cf907377702bebcc8e8746662f6cc
Tomoya Sawada <stomoya0110@gmail.com>
2026-05-18 17:23:26 +0900

    feat: better imports for client registry funcs

--- src/nekomata/__init__.py
+++ src/nekomata/__init__.py
@@ -1,6 +1,7 @@
 """LLM dispatcher."""
 
 from .clients.factory import create_client
+from .clients.registry import list_client_keys, register_client
 from .dispatcher.dispatcher import AsyncLLMDispatcher
 from .dispatcher.sync import SyncLLMDispatcher
 from .types.integrations import ChatCompletionResponse, ChatCompletionStatus
@@ -13,4 +14,6 @@ __all__ = [
     'SyncLLMDispatcher',
     '__version__',
     'create_client',
+    'list_client_keys',
+    'register_client',
 ]
--- src/nekomata/clients/__init__.py
+++ src/nekomata/clients/__init__.py
@@ -1,7 +1,10 @@
 """LLM clients."""
 
 from .factory import create_client
+from .registry import list_client_keys, register_client
 
 __all__ = [
     'create_client',
+    'list_client_keys',
+    'register_client',
 ]
