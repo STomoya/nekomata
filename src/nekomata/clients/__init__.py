e695887ba71d910ce9bb3cc17815aa28cc71d0ac
Tomoya Sawada <stomoya0110@gmail.com>
2026-07-04 19:33:06 +0900

    feat: add polling helper

--- src/nekomata/clients/__init__.py
+++ src/nekomata/clients/__init__.py
@@ -3,10 +3,12 @@
 from .base import BatchAPIPlugin, ClientABC
 from .factory import create_client
 from .registry import list_client_keys, register_client
+from .helpers import apoll_batch
 
 __all__ = [
     'BatchAPIPlugin',
     'ClientABC',
+    'apoll_batch',
     'create_client',
     'list_client_keys',
     'register_client',
